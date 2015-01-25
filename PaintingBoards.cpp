8a3286d7406978fc64580f510a4fb45a85f8efb5
Arshdeep <arshdeep.singh86@gmail.com>
2015-01-25 21:30:46 +0530

    binary search + bitwise + dp

--- PaintingBoards.cpp
+++ PaintingBoards.cpp
@@ -13,129 +13,147 @@ using namespace std;
 /* http://community.topcoder.com/stat?c=problem_statement&pm=8516&rd=11124 */
 #define SIZEOFARRAY(s) sizeof(s)/sizeof(s[0])
 #define REP(i,n) for(int i=0;i<(n);++i)
-#define LOOP(i,v,n) for(int i=v;i<(n);++i)
+#define LOOP(i,v,n) for(i=v;i<(n);++i)
 #define LL long long
 #define memSet(m, v) memset(m, v, sizeof(m))
 
+int howFar[51][15];
+//bool canpaint[51][1<<15];
 
 class PaintingBoards
 {
-    int mBoardLen;
-    int mPainters;
-    map<pair<LL, int>, double> memo;
-    double solve(int l, LL mask, const vector<int> &boardLength, const vector<int> &painterSpeed)
-    {
-        if (mask == ((1 << mBoardLen) - 1)) return 0;
-        
-        map<pair<LL, int>, double>::iterator itr = memo.find(make_pair(mask, l));
-        
-        if (itr != memo.end()) return itr->second;
-        
-        
-        double ret = INT_MAX;
-        
-        REP(i, mPainters)
-        {
-            if ((l & (1 << i)) != 0) continue;
-            
-            double min = 0.0;
-            double temp = 0.0;
-            REP(j, mBoardLen)
-            {
-                if ((mask & (1 << j)) != 0)
-                {
-                    min = 0.0;
-                    continue;
-                }
-                
-                min += (1.0 / (double)painterSpeed[i]) * boardLength[j];
-                LL m = 1;
-                
-                if (j != 0)
-                    m = ((LL)1 << (j + 1)) - 1;
-                
-                temp = std::max(min, solve(j | (1 << i), mask | m, boardLength, painterSpeed));
-                ret = std::min(ret, temp);
+	int mBoardLen;
+	int mPainters;
 
-            }
-            
-        }
-        memo.insert(make_pair(make_pair(mask, l), ret));
-        
-        return ret;
-    }
+	bool canPaint(const vector<int> &boardLength, const vector<int> &painterSpeed, double time)
+	{
+		int k = 0;
+		REP(i, mBoardLen)
+		{
+			REP(j, mPainters)
+			{
+				int dist = 0;
+				double maxDist = time * painterSpeed[j];
+				LOOP(k, i, mBoardLen)
+				{
+					dist += boardLength[k];
+					if (maxDist < dist) break;
+				}
+				howFar[i][j] = k;
+			}
+		}
+		vector< vector <int> > canpaint(mBoardLen + 1, vector<int>(1 << mPainters));
+		canpaint[0][0] = true;
+		REP(i, mBoardLen)
+		{
+			REP(j, (1 << mPainters))
+			{
+				if (!canpaint[i][j]) continue;
+
+				REP(k, mPainters)
+				{
+					if ((j & (1 << k)) == 0)
+					{
+						canpaint[ howFar[i][k] ][j | (1 << k)] = true;
+					}
+				}
+			}
+		}
+		int j = 0;
+		REP(j, (1 << mPainters))
+		{
+			if (canpaint[mBoardLen][j])
+				return true;
+		}
+
+		return false;
+	}
 public:
-    double minimalTime(vector<int> boardLength, vector<int> painterSpeed)
-    {
+	double minimalTime(vector<int> boardLength, vector<int> painterSpeed)
+	{
+		memSet(howFar, 0);
+		//memSet(canpaint, 0);
 
-        mBoardLen = (int)boardLength.size();
-        mPainters = (int)painterSpeed.size();
+		mBoardLen = (int)boardLength.size();
+		mPainters = (int)painterSpeed.size();
         
-        return solve(0, 0, boardLength, painterSpeed);
-    }
+		double start = 0;
+		double end = 50000;
+
+		while (end - start > 1e-11)
+		{
+			double mid = start + (end - start) / 2;
+
+			if (canPaint(boardLength, painterSpeed, mid))
+				end = mid;
+			else
+				start = mid;
+		}
+		return end;
+	}
 };
 
 void TEST(vector<int> boardLength, vector<int> painterSpeed, double expected)
 {
-    clock_t start, end;
-    double cpu_time_used;
+	clock_t start, end;
+	double cpu_time_used;
     
-    start = clock();
-    PaintingBoards paintingBoards;
-    double result = paintingBoards.minimalTime(boardLength, painterSpeed);
+	start = clock();
+	PaintingBoards paintingBoards;
+	double result = paintingBoards.minimalTime(boardLength, painterSpeed);
+    cout<<result<< " "<< expected<<endl;
+	//assert(result - expected <= 1e-12);
     
-    assert(result == expected);
-    
-    end = clock();
-    cpu_time_used = ((double)(end - start));
-    cout << "Time taken : " << cpu_time_used << endl;
+	end = clock();
+	cpu_time_used = ((double)(end - start));
+	cout << "Time taken : " << cpu_time_used << endl;
 }
 
 template <class T>
 vector<T> convert(T *list, int n)
 {
-    vector<T> ret;
+	vector<T> ret;
     
-    for (int i = 0; i< n; ++i)
-    {
-        ret.push_back(list[i]);
-    }
+	for (int i = 0; i< n; ++i)
+	{
+		ret.push_back(list[i]);
+	}
     
-    return ret;
+	return ret;
 }
 
 void Test1()
 {
-    int boardLength[] = {5,3,2};
-    int painterSpeed[] = {2,3,5};
-    TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
-         convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 1.0);
+	int boardLength[] = {5,3,2};
+	int painterSpeed[] = {2,3,5};
+	TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
+			convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 1.0);
 }
 
 void Test2()
 {
-    int boardLength[] = {1,2,1,4,2,1,1};
-    int painterSpeed[] = {1,2,3};
-    TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
-         convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 2.0);
+	int boardLength[] = {1,2,1,4,2,1,1};
+	int painterSpeed[] = {1,2,3};
+	TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
+			convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 2.0);
 }
 
 void Test3()
 {
-    int boardLength[] = {3,3,20};
-    int painterSpeed[] = {9,1};
-    TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
-         convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 2.888888888888889);
+	int boardLength[] = {3,3,20};
+	int painterSpeed[] = {9,1};
+	TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
+			convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 2.888888888888889);
 }
 
 void Test4()
 {
-    int boardLength[] = {40, 46, 82, 89, 33, 46, 3, 50, 95,
-        81, 69, 40, 94, 93, 90, 98, 17, 34,
-        45, 18, 93, 28, 43, 38, 35};
-    int painterSpeed[] = {49, 51, 35, 27, 48, 36, 54, 10};
-    TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
-         convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 4.686274509803922);
+	int boardLength[] = {40, 46, 82, 89, 33, 46, 3, 50, 95,
+		81, 69, 40, 94, 93, 90, 98, 17, 34,
+		45, 18, 93, 28, 43, 38, 35};
+	int painterSpeed[] = {49, 51, 35, 27, 48, 36, 54, 10};
+	TEST(convert(boardLength, SIZEOFARRAY(boardLength)),
+			convert(painterSpeed, SIZEOFARRAY(painterSpeed)), 4.686274509803922);
 }
 
 void Test5()
@@ -163,16 +181,17 @@ void Test10()
 }
 int main()
 {
-    Test1();
-    Test2();
-    Test3();
-    Test4();
-    Test5();
-    Test6();
-    Test7();
-    Test8();
-    Test9();
-    Test10();
-    cout << "success";
-    return 0;
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	Test7();
+	Test8();
+	Test9();
+	Test10();
+	getchar();
+	cout << "success";
+	return 0;
 }
